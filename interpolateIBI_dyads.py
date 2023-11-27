9d9c3974f02e429fa669834240e56f5f87dffe00
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2023-11-27 19:17:46 +0100

    dyad interpolation works

--- interpolateIBI_dyads.py
+++ interpolateIBI_dyads.py
@@ -1,7 +1,7 @@
 '''
 Transform a discrete list of inter-beat intervals (IBIs) to a continuous function using cubic spline interpolation.
 The continuous function is sampled at a fixed sampling interval. 
-The newly obtained samples are then scaled so that their sum matches the sum of the original IBIs.
+The newly obtained samples are then scaled so that their sum matches the length of the original recording.
 
 by Moritz Wunderwald, 2023
 '''
@@ -39,23 +39,41 @@ for input_path in input_paths:
 
     # read ibi data
     input_data_df = pd.read_csv(input_path)
-    t_ms = input_data_df['t_ms'].to_numpy()
-    ibi_ms = input_data_df[' ibi_ms'].to_numpy()
+    t_ms_combined = input_data_df['t_ms'].to_numpy()
+    ibi_ms_combined = input_data_df[' ibi_ms'].to_numpy()
+    ecg_markers = input_data_df[' ecg_id'].to_numpy()
+
+    # make indices to split data
+    sampleIsEcg1 = ecg_markers == ' ecg1'
+    sampleIsEcg2 = ecg_markers == ' ecg2'
+    
+    # split data
+    t_ms_ecg1 = t_ms_combined[sampleIsEcg1]
+    ibi_ms_ecg1 = ibi_ms_combined[sampleIsEcg1]
+    t_ms_ecg2 = t_ms_combined[sampleIsEcg2]
+    ibi_ms_ecg2 = ibi_ms_combined[sampleIsEcg2]
 
     # Create a cubic spline interpolation
-    cs = CubicSpline(t_ms, ibi_ms)
+    cs_ecg1 = CubicSpline(t_ms_ecg1, ibi_ms_ecg1)
+    cs_ecg2 = CubicSpline(t_ms_ecg2, ibi_ms_ecg2)
 
     # Generate time grid for interpolation
-    t_ms_interpl = np.arange(min(t_ms), max(t_ms), INTERVAL_MS)
+    t_ms_interpl = np.arange(min(t_ms_combined), max(t_ms_combined), INTERVAL_MS)
 
-    # Interpolate values at the finer time points
-    ibi_ms_interpl = cs(t_ms_interpl)
+    # Get interpolated sample values
+    ibi_ms_interpl_ecg1 = cs_ecg1(t_ms_interpl)
+    ibi_ms_interpl_ecg2 = cs_ecg2(t_ms_interpl)
 
-    # scale interpolated ibi so that sum of ibis matches um of original ibis
-    sum_ibi_original = sum(ibi_ms)
-    sum_ibi_interpl = sum(ibi_ms_interpl)
-    scl = sum_ibi_original / sum_ibi_interpl
-    ibi_ms_interpl_scl = ibi_ms_interpl * scl
+    # make scaling factors
+    recording_length = t_ms_combined[len(t_ms_combined)-1] - t_ms_combined[0]
+    sum_ibi_interpl_ecg1 = sum(ibi_ms_interpl_ecg1)
+    sum_ibi_interpl_ecg2 = sum(ibi_ms_interpl_ecg2)
+    scl_ecg1 = recording_length / sum_ibi_interpl_ecg1
+    scl_ecg2 = recording_length / sum_ibi_interpl_ecg2
+    
+    # scale interpolated ibi so that sum of ibis matches sum of original ibis
+    ibi_ms_interpl_scl_ecg1 = ibi_ms_interpl_ecg1 * scl_ecg1
+    ibi_ms_interpl_scl_ecg2 = ibi_ms_interpl_ecg2 * scl_ecg2
 
     # make output path
     input_file_w_extension = os.path.basename(input_path)
@@ -64,5 +82,9 @@ for input_path in input_paths:
     output_path = os.path.join(output_dir_path, output_file)
 
     # write interpolated and scaled ibi data to a csv file
-    csv_df = pd.DataFrame({'t_ms': t_ms_interpl, 'ibi_ms': ibi_ms_interpl_scl}).round(4)
+    csv_df = pd.DataFrame({
+        't_ms': t_ms_interpl, 
+        'ibi_ms_ecg1': ibi_ms_interpl_scl_ecg1, 
+        'ibi_ms_ecg2': ibi_ms_interpl_scl_ecg2, 
+    }).round(4)
     csv_df.to_csv(output_path, index=False)
